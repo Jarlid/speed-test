eb1fcdac287e6c7b03fbcb1947103dd71f90feb8
Jarlid <korkiniostudymail@gmail.com>
2023-05-17 23:37:27 +0300

    GUI added

--- client.py
+++ client.py
@@ -1,6 +1,45 @@
+import tkinter
 from engine import *
+from config import DEFAULT_IP, DEFAULT_PORT
+DEFAULT_BATCH_NUM = 5
 
-client = start_client()
 
-batch_num = int(input('BATCH_NUM: '))
-timed_send(client, DEFAULT_ADDRESS, batch_num)
+def button_pressed():
+    send_button['text'] = 'Отправка...'
+    root.after(1, send_time)
+
+
+def send_time():
+    ip = ip_entry.get()
+    port = int(port_entry.get())
+    address = (ip, port)
+    batch_num = int(pack_entry.get())
+
+    client = start_client(address)
+    timed_send(client, address, batch_num)
+
+    send_button['text'] = 'Отправить'
+
+
+root = tkinter.Tk()
+root.title('UDP sender' if MODE == Connection.UDP else 'TCP sender')
+
+tkinter.Label(text='IP получателя:').grid(row=0, column=0, sticky=tkinter.W, pady=5, padx=5)
+ip_entry = tkinter.Entry()
+ip_entry.insert(0, DEFAULT_IP)
+ip_entry.grid(row=0, column=1, padx=5)
+
+tkinter.Label(text='Порт получателя:').grid(row=1, column=0, sticky=tkinter.W, pady=5, padx=5)
+port_entry = tkinter.Entry()
+port_entry.insert(0, str(DEFAULT_PORT))
+port_entry.grid(row=1, column=1)
+
+tkinter.Label(text='Кол-во пакетов:').grid(row=2, column=0, sticky=tkinter.W, pady=5, padx=5)
+pack_entry = tkinter.Entry()
+pack_entry.insert(0, str(DEFAULT_BATCH_NUM))
+pack_entry.grid(row=2, column=1)
+
+send_button = tkinter.Button(text='Отправить', command=button_pressed)
+send_button.grid(row=3, column=0, columnspan=2, pady=5, padx=5)
+
+root.mainloop()
--- server.py
+++ server.py
@@ -1,33 +1,77 @@
+import tkinter
 from engine import *
-from config import BATCH_SIZE, SERVER_TIMEOUT
+from config import DEFAULT_IP, DEFAULT_PORT, BATCH_SIZE, SERVER_TIMEOUT
 from math import inf
 MIL = 1000000
 
-server = start_server()
-servcon = establish_connection(server)
 
-batch_set = set()
-batch_num = inf
-send_time = None
-last_time = None
-started = False
+def button_pressed():
+    recv_button['text'] = 'Получение...'
+    root.after(1, recv_time)
 
-try:
-    while len(batch_set) < batch_num:
-        send_time, batch_num, curr_batch = parse_package(use_recv(servcon)[0])
-        last_time = datetime.now()
-        batch_set.add(curr_batch)
-        if not started:
-            servcon.settimeout(SERVER_TIMEOUT)
-            started = True
-except socket.timeout:
-    pass
 
-time_delta = last_time - send_time
-time_delta = MIL * time_delta.seconds + time_delta.microseconds
-speed = BATCH_SIZE * len(batch_set) / time_delta
+def recv_time():
+    ip = ip_entry.get()
+    port = int(port_entry.get())
+    address = (ip, port)
 
-print('Packages sent:    ', batch_num)
-print('Packages received:', len(batch_set))
-print('Total time:', time_delta / MIL, 's')
-print('Speed:', speed, 'MB/s')
+    server = start_server(address)
+    servcon = establish_connection(server)
+
+    batch_set = set()
+    batch_num = inf
+    send_time = None
+    last_time = None
+    started = False
+
+    try:
+        while len(batch_set) < batch_num:
+            send_time, batch_num, curr_batch = parse_package(use_recv(servcon)[0])
+            last_time = datetime.now()
+            batch_set.add(curr_batch)
+            if not started:
+                servcon.settimeout(SERVER_TIMEOUT)
+                started = True
+    except socket.timeout:
+        pass
+
+    time_delta = last_time - send_time
+    time_delta = MIL * time_delta.seconds + time_delta.microseconds
+    speed = BATCH_SIZE * len(batch_set) / time_delta
+
+    pack_label['text'] = f'{len(batch_set)} из {batch_num}'
+    time_label['text'] = f'{time_delta / MIL} c'
+    speed_label['text'] = '%.6f МБ/c' % speed
+
+    recv_button['text'] = 'Получить'
+
+
+root = tkinter.Tk()
+root.title('UDP receiver' if MODE == Connection.UDP else 'TCP receiver')
+
+tkinter.Label(text='IP получения:').grid(row=0, column=0, sticky=tkinter.W, pady=5, padx=5)
+ip_entry = tkinter.Entry()
+ip_entry.insert(0, DEFAULT_IP)
+ip_entry.grid(row=0, column=1, padx=5)
+
+tkinter.Label(text='Порт получения:').grid(row=1, column=0, sticky=tkinter.W, pady=5, padx=5)
+port_entry = tkinter.Entry()
+port_entry.insert(0, str(DEFAULT_PORT))
+port_entry.grid(row=1, column=1)
+
+tkinter.Label(text='Пакетов получено:').grid(row=2, column=0, sticky=tkinter.W, pady=5, padx=5)
+pack_label = tkinter.Label(text='-')
+pack_label.grid(row=2, column=1, sticky=tkinter.W, padx=5)
+
+tkinter.Label(text='Время:').grid(row=3, column=0, sticky=tkinter.W, pady=5, padx=5)
+time_label = tkinter.Label(text='-')
+time_label.grid(row=3, column=1, sticky=tkinter.W, padx=5)
+
+tkinter.Label(text='Скорость:').grid(row=4, column=0, sticky=tkinter.W, pady=5, padx=5)
+speed_label = tkinter.Label(text='-')
+speed_label.grid(row=4, column=1, sticky=tkinter.W, padx=5)
+
+recv_button = tkinter.Button(text='Получить', command=button_pressed)
+recv_button.grid(row=5, column=0, columnspan=2, pady=5, padx=5)
+
+root.mainloop()
